e0160894c154dfd468cf0dd0c6bf7d1ac1d679dc
louJ-dev <jericfactor@gmail.com>
2025-10-07 01:24:54 +0800

    add struct Queue and main function

--- queue.c
+++ queue.c
@@ -1,2 +1,13 @@
+#include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+    int size;
+    int capacity;
+    int *collection;
+} Queue;
+
+int main() {
+    printf("Hello World\n");
+    return 0;
+}
